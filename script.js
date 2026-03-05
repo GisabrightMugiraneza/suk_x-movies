8e6fa4dfcf9b08e2a3e780790a08dcfac71958c0
GisabrightMugiraneza <brightgisa1@gmail.com>
2026-03-04 22:40:17 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const apiKey = "27d7234cc0a8643d0b6d28fa1a7517aa";
+
+async function searchMovies(){
+
+let query = document.getElementById("searchInput").value;
+
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+
+let response = await fetch(url);
+let data = await response.json();
+
+let moviesDiv = document.getElementById("movies");
+moviesDiv.innerHTML = "";
+
+data.results.forEach(movie => {
+
+let poster = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+
+moviesDiv.innerHTML += `
+<div class="movie">
+<img src="${poster}">
+<h3>${movie.title}</h3>
+<p>⭐ ${movie.vote_average}</p>
+</div>
+`;
+
+});
+
+}
