66e2e6bb8fc2de64bae2a03a78094a870bd18076
GisabrightMugiraneza <brightgisa1@gmail.com>
2026-03-10 20:33:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
 const apiKey = "27d7234cc0a8643d0b6d28fa1a7517aa";
 
-async function searchMovies(){
+async function searchMovies() {
 
-let query = document.getElementById("searchInput").value;
+    const query = document.getElementById("searchInput").value;
 
-let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+    if(query === ""){
+        alert("Please enter a movie name");
+        return;
+    }
 
-let response = await fetch(url);
-let data = await response.json();
+    const url =
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
 
-let moviesDiv = document.getElementById("movies");
-moviesDiv.innerHTML = "";
+    const response = await fetch(url);
+    const data = await response.json();
 
-data.results.forEach(movie => {
+    displayResults(data.results);
+}
 
-let poster = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+function displayResults(movies){
 
-moviesDiv.innerHTML += `
-<div class="movie">
-<img src="${poster}">
-<h3>${movie.title}</h3>
-<p>⭐ ${movie.vote_average}</p>
+    const grid = document.getElementById("resultsGrid");
+    const section = document.getElementById("searchResults");
 
+    grid.innerHTML = "";
 
-</div>
-`;
-});
-  function playMovie(videoId) {
-  const player = document.getElementById("moviePlayer");
-  player.src = "https://www.youtube.com/embed/" + videoId;
+    if(movies.length === 0){
+        grid.innerHTML = `<div class="no-results">No movies found</div>`;
+        return;
+    }
+
+    movies.forEach(movie => {
+
+        const poster = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : "https://via.placeholder.com/300x450";
+
+        const card = `
+        <div class="movie-card">
+            <div class="movie-poster">
+                <img src="${poster}">
+            </div>
+
+            <div class="movie-info">
+                <h3>${movie.title}</h3>
+                <div class="movie-meta">
+                    <span>${movie.release_date || "N/A"}</span>
+                    <span class="rating">★ ${movie.vote_average}</span>
+                </div>
+            </div>
+        </div>
+        `;
+
+        grid.innerHTML += card;
+
+    });
+
+    section.classList.add("active");
 }
 
 
-}
+
+
+
 
 
 
